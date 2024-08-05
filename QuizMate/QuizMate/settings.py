2d61c11ba1076e7c33cc4ee15970033137da638e
rSchleckser <rickyschleckser@gmail.com>
2024-08-05 00:59:14 -0700

    feat: add dependancies and setup postgres database

--- QuizMate/QuizMate/settings.py
+++ QuizMate/QuizMate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'QuizMate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizmate',
+        'USER': 'quizmate_user',
+        'PASSWORD': 'quizmate',
+        'HOST': 'localhost'
     }
 }
 
