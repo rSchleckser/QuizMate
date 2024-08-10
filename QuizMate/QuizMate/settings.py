4dbe11fd8ff43ce456fe975ed7a3c9667b29aff7
rSchleckser <rickyschleckser@gmail.com>
2024-08-10 15:27:31 -0700

    feat: add info necessary to deploy

--- QuizMate/QuizMate/settings.py
+++ QuizMate/QuizMate/settings.py
@@ -125,7 +125,7 @@ LOGIN_URL = '/login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
