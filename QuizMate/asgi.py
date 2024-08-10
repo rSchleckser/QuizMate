06fdfd092812a286577d663d254640c1a88e02f5
rSchleckser <rickyschleckser@gmail.com>
2024-08-10 16:51:37 -0700

    feat fix engine error

--- QuizMate/asgi.py
+++ QuizMate/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuizMate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courses.settings')
 
 application = get_asgi_application()
