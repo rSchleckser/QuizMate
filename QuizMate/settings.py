aeadbdea4d5e832eb908e115feca4d7052c97d2c
rSchleckser <rickyschleckser@gmail.com>
2024-08-19 23:37:12 -0700

    feat: remove comments from settings.py

--- QuizMate/settings.py
+++ QuizMate/settings.py
@@ -27,8 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -138,8 +136,7 @@ AUTH_USER_MODEL = 'courses.CustomUser'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+ 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
